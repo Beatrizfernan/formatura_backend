d3b560000d34ce87618b98dfde686425313d7f02
Beatriz Fernandes <beatrizfernandesssouz@gmail.com>
2025-11-02 20:14:24 -0300

    segundo commit

--- app/__init__.py
+++ app/__init__.py
@@ -8,5 +8,8 @@ from config import MONGO_URI
 def create_app():
     app = Flask(__name__)
     connect(host=MONGO_URI)  
+    @app.route("/")
+    def home():
+        return "Backend rodando no Render! 🚀"
     
     return app
