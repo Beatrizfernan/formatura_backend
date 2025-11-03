de4ed22effb4d4b176ec54153057f36108c84d9d
Beatriz Fernandes <beatrizfernandesssouz@gmail.com>
2025-11-02 21:00:41 -0300

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from mongoengine import connect
-
+from flask_cors import CORS
 from config import MONGO_URI
 
-
+from app.routes.local_routes import local_bp
+from app.routes.formatura_routes import formatura_bp
+from app.routes.planilha_routes import planilha_bp
 
 def create_app():
     app = Flask(__name__)
@@ -11,5 +13,12 @@ def create_app():
     @app.route("/")
     def home():
         return "Backend rodando no Render! 🚀"
+    CORS(app)
+
+    app.register_blueprint(local_bp)
+    app.register_blueprint(formatura_bp)
+    app.register_blueprint(planilha_bp)
+
+
     
     return app
